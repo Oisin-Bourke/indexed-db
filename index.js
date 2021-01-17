cf18e6edb54bcbd2e9fdf1d5737a7db5f3ed5471
Oisin <obourke@gmail.com>
2021-01-17 15:11:41 +0000

    Added caching with Cache API

--- index.js
+++ index.js
@@ -142,4 +142,11 @@ window.onload = () => {
             };  
         };
     };
+
+    // Register service worker to control site offline
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js')
+            .then(() => console.log('Service worker registered'));
+    }
 };
